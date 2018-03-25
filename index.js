1d605223670603c4a6df0d36ca754cee87a21eed
Robert H Boscacci <cinemarob1@gmail.com>
2018-03-25 11:59:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function removeFromCart(item) {
   }
 
 function placeOrder(cardNumber) {
-  if(cardNumber===''){
-    return('Sorry, we don\'t have a credit card on file for you.');
+  if(cardNumber===){
+    console.log('Sorry, we don\'t have a credit card on file for you.');
   }
 }
