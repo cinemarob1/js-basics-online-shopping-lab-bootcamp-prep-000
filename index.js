e170625133e5aadf677a00acabd264b5628ecffd
Robert H Boscacci <cinemarob1@gmail.com>
2018-03-25 11:26:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,9 +48,8 @@ function total() {
 function removeFromCart(item) {
   for(var i=0; i<cart.length;i++){
     if (cart[i].itemName===item){
-      cart.prototype.splice(i, 1);
-      console.log(cart);
-      return cart;
+      cart.splice(i, 1);
+      break;
     }
     else{
       return('That item is not in your cart.');
