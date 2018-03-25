641d998d77c38b3f5de2215a537af353c9f5547b
Robert H Boscacci <cinemarob1@gmail.com>
2018-03-25 10:51:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function viewCart() {
     return('In your cart, you have ' + cart[0].itemName + ' at $' + cart[0].itemPrice + '.');
   }
   else if (cart.length===2){
-    console.log('In your cart, you have '+ cart[0].itemName +' at $'+ cart[0].itemPrice + ', and '+ cart[1].itemName +' at $'+ cart[1].itemPrice + '.')
+    return('In your cart, you have '+ cart[0].itemName +' at $'+ cart[0].itemPrice + ', and '+ cart[1].itemName +' at $'+ cart[1].itemPrice + '.');
   }
 }
 
